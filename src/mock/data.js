b1fbbcd01fda764651293435ecdad07d6e6b89e3
Diego23242 <94395761+Diego23242@users.noreply.github.com>
2025-05-04 00:08:28 -0700

    cambios importantes

--- src/mock/data.js
+++ src/mock/data.js
@@ -1,47 +1,47 @@
 const products = [
   {
     id: 1,
-    name: "Vestido Floral Midi",
+    name: "Funda de felpa",
     price: 300,
-    image: "https://images.unsplash.com/photo-1585487000160-6ebcfceb0d03?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=80",
-    description: "Vestido de verano con estampado floral y corte midi",
-    category: "ropa",
+    image: "/img/fondo1.jpeg",
+    description: "Protector de lente de expresión de felpa y estilo de moda.",
+    category: "Funda",
     isPopular: true,
     discount: 15
   },
   {
     id: 2,
-    name: "Zapatos Plataforma",
-    price: 39.99,
-    image: "https://images.unsplash.com/photo-1549298916-b41d501d3772?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=80",
-    description: "Zapatos de plataforma con diseño moderno y cómodo",
-    category: "calzado",
+    name: "Funda Linda",
+    price: 65,
+    image: "/img/fondo2.jpeg",
+    description: "Funda linda para teléfono con decoración de orejas de conejo.",
+    category: "Funda",
     isNew: true
   },
   {
     id: 3,
-    name: "Bolso Bandolera",
-    price: 24.99,
-    image: "https://images.unsplash.com/photo-1590874103328-eac38a683ce7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=80",
-    description: "Bolso bandolera multifuncional con múltiples compartimentos",
+    name: "Funda de Stitch",
+    price: 65,
+    image: "/img/fondo3.jpeg",
+    description: "Funda adorable y para llevar siempre contigo.",
     category: "accesorios",
     isPopular: true
   },
   {
     id: 4,
-    name: "Gafas de Sol Oversize",
+    name: "Funda de Gato",
     price: 19.99,
-    image: "https://images.unsplash.com/photo-1511499767150-a48a237f0083?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=80",
-    description: "Gafas de sol con montura oversize y protección UV400",
+    image: "/img/fondo4.jpeg",
+    description: "Funda adorable y para llevar siempre contigo.",
     category: "accesorios",
     discount: 20
   },
   {
     id: 5,
-    name: "Gafas de playa",
+    name: "Funda de Corazón",
     price: 30.99,
-    image: "https://images.unsplash.com/photo-1511499767150-a48a237f0083?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=80",
-    description: "Gafas de sol con montura oversize y protección UV400",
+    image: "/img/fondo5.jpeg",
+    description: "Funda adorable y para llevar siempre contigo.",
     category: "accesorios",
     discount: 20
   }
